8e626dcb07817bd69a51bfc1984914c1b0072ce6
Stanislas Polu <polu.stanislas@gmail.com>
2014-09-18 10:40:11 -0700

    Updated visu_nn for nicer graphs

--- visu/visu_nn.js
+++ visu/visu_nn.js
@@ -15,20 +15,20 @@ var path = require('path');
 var fs = require('fs');
 
 //
-// The `visu` tool takes as argument a neural network and generates an html
+// The `visu_nn` tool takes as argument a neural network and generates an html
 // page rendering the neural network using SVG
 //
-// The `visu` command takes a filename as argument containing the neural network
-// string as returned by the `to_string` method. 
+// The `visu_nn` command takes a filename as argument containing the neural 
+// network string as returned by the `to_string` method. 
 // 
 
 var VISU_WIDTH = 800;
 var VISU_HEIGHT = 600;
-var W_THRESHOLD = 1;
+var W_THRESHOLD = 0.8;
 var DEFAULT_WMAX = 3;
 var DEFAULT_WMIN = -3;
 
-var PRE_RUN = true;
+var PRE_RUN = false;
 
 if(process.argv.length < 3) {
   console.log('Usage: `visu nn.out`');
@@ -80,7 +80,7 @@ if(PRE_RUN) {
   console.log('bias : ' + bias);
   console.log('Wmax : ' + Wmax);
   console.log('Wmin : ' + Wmin);
-  process.exiT(0);
+  process.exit(0);
 }
 
 /* Finally we create an html file containing SVG data based on the network   */
@@ -102,18 +102,21 @@ for(var l = 0; l < L.length; l ++) {
 
         if(w > W_THRESHOLD || w < -W_THRESHOLD) {
           var r = 255;
+          var g = 255;
           var b = 255;
           if(w > 0) {
-            b = Math.max(Math.floor(255 - (w/3 * 255), 0));
+            r = Math.max(Math.floor(255 - (w/Wmax * 255), 0));
+            g = Math.max(Math.floor(255 - (w/Wmax * 255), 0));
           }
           if(w < 0) {
-            r = Math.max(Math.floor(255 - (-w/3 * 255), 0));
+            b = Math.max(Math.floor(255 - (w/Wmin * 255), 0));
+            g = Math.max(Math.floor(255 - (w/Wmin * 255), 0));
           }
           console.log('<line x1="' + x1 + 
                       '" y1="' + y1 + 
                       '" x2="' + x2 + 
                       '" y2="' + y2 + 
-                      '" style="stroke:rgba(' + r + ',255,' + b + ', 0.5);stroke-width:1" />');
+                      '" style="stroke:rgba(' + r + ',' + g + ',' + b + ', 0.5);stroke-width:1" />');
         }
       }
     }
